87c8570681e891f70fb1a0ba9b9960b6405d5811
William Milanez <william.milanez@outlook.com>
2025-10-31 00:33:06 -0300

    formatando o valor total

--- scripts.js
+++ scripts.js
@@ -121,7 +121,7 @@ function updateTotals() {
             const itemAmount = items[item].querySelector(".expense-amount")
 
             // Remove caracteres não numéricos e substitui a vírgula pelo ponto
-            let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",", ".")
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".")
 
             // Converte o valor para float
             value = parseFloat(value)
@@ -135,8 +135,18 @@ function updateTotals() {
             total += Number(value)
         }
 
-        //
-        expensesTotal.textContent = total
+        // Cria a span pra adicionar o R$ formatado
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+        // Formata o valor e remove o R$ que será exibido pela small com um estilo customizado
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+        // Limpa o conteúdo do elemento
+        expensesTotal.innerHTML = ""
+
+        // Adiciona o símbolo da moeda e o valor total formatado
+        expensesTotal.append(symbolBRL, total)
 
     } catch (error) {
         alert("Não foi possível atualizar os totais.")
