e1782ab54d7737a06e319415251c6d3e233df8ea
William Milanez <william.milanez@outlook.com>
2025-10-30 23:12:14 -0300

    criando a função para adicionar uma nova despesa

--- scripts.js
+++ scripts.js
@@ -41,4 +41,15 @@ form.onsubmit = (event) => {
         amount: amount.value,
         created_at: new Date(),
     }
+
+    // Chama a função que irá adicionar o item na lista
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
 }
