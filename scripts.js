2e2ac2bfd9e11623b7e4fe55b36f2120d65b0f04
William Milanez <william.milanez@outlook.com>
2025-10-30 23:33:13 -0300

    exibindo a quantidade de itens da lista

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const category = document.getElementById("category")
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector("ul")
+const expenseQuantity = document.querySelector("aside header p span")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -49,6 +50,7 @@ form.onsubmit = (event) => {
     expenseAdd(newExpense)
 }
 
+// Adiciona um novo item na lista
 function expenseAdd(newExpense) {
     try {
         // Cria o elemento para a adicionar o item (li) na lista (ul)
@@ -86,15 +88,32 @@ function expenseAdd(newExpense) {
         removeIcon.setAttribute("src", "img/remove.svg")
         removeIcon.setAttribute("alt", "Remover")
 
-
         // Adiciona as informações no item
         expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
         // Adiciona o item na lista
         expenseList.append(expenseItem)
 
+        // Atualiza os totais
+        updateTotals()
+
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
+}
+
+// Atualiza os totais
+function updateTotals() {
+    try {
+        // Recupera todos os itens (li) da lista (ul)
+        const items = expenseList.children
+
+        // Atualiza a quantidade de itens da lista
+        expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+    } catch (error) {
+        alert("Não foi possível atualizar os totais.")
         console.log(error)
     }
 }
